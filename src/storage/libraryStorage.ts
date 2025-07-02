b7351a29b40e4576aba3663bb1ce066e146decd5
Angela Carvalho <angelalopo@outlook.com>
2025-07-01 21:44:54 -0300

    add: estrutura final

---
+++ src/storage/libraryStorage.ts
@@ -0,0 +1,35 @@
+export interface Book {
+  id: string;
+  title: string;
+  content: string;
+  status: string;
+  author: string;
+  created_at: string;
+}
+
+class BookStorage {
+  
+  private static instance: BookStorage;
+  
+  private books: Book[] = [];
+
+  private constructor() {}
+
+  public static getInstance(): BookStorage {
+    if (!BookStorage.instance) {
+      BookStorage.instance = new BookStorage();
+    }
+    return BookStorage.instance;
+  }
+
+  public add(book: Book): void {
+    this.books.push(book);
+  }
+  
+  public getAll(): Book[] {
+    return this.books;
+  }
+
+}
+
+export default BookStorage.getInstance();
