519b1fe8dcaa26037d635dbd02c2b521e3f7c450
Angela Carvalho <angelalopo@outlook.com>
2025-07-30 16:02:51 -0300

    feat: add welcomeUser and createUser functions to userController

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,1 +1,17 @@
 import { Request, Response } from 'express'; 
+import userService from '../factories/userFactory'; 
+
+export const welcomeUser = async (_: Request, res: Response): Promise<void> => {
+  res.status(200).json('Bem-vindo à API de Usuários!');
+};
+
+export const createUser = async (req: Request, res: Response): Promise<void> => {
+  const { name, email, login, password } = req.body;
+
+  try {
+    const newUser = await userService.create({ name, email, login, password });
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: 'Erro ao criar usuário', error });
+  }
+};
